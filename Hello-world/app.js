49ec2ceaba3ec708ab0c0f4518f79296504ee65a
osama823 <osama823@gmail.com>
2018-09-06 04:16:09 -0700

    first commit

---
+++ Hello-world/app.js
@@ -0,0 +1,14 @@
+
+var h1=React.createElement('h1',null,'Hello World!');
+var content=document.getElementById('app');
+
+ReactDOM.render(h1,content);
+
+// ReactDOM.render(
+//   React.createElement(
+//     'h1',
+//     null,
+//     'Hello World!'
+//   ),
+//   document.getElementById('app')
+// );
