2a18555fa9831ab5f1672370d54046b8fddb9ecf
osama823 <osama823@gmail.com>
2018-09-06 06:03:58 -0700

    cutom element with baby steps

--- Hello-world/app.js
+++ Hello-world/app.js
@@ -1,9 +1,9 @@
-
+// neat code
 var h1=React.createElement('h1',null,'Hello World!');
 var content=document.getElementById('app');
 
-ReactDOM.render(h1,content);
-
+//ReactDOM.render(h1,content);
+// basic code
 // ReactDOM.render(
 //   React.createElement(
 //     'h1',
@@ -12,3 +12,13 @@ ReactDOM.render(h1,content);
 //   ),
 //   document.getElementById('app')
 // );
+
+// anchor
+var a=React.createElement('a',{href:'http://www.google.com',target:'_blank',title:'Google Link'},'Google');
+//var link=document.getElementById('link');
+
+ReactDOM.render(React.createElement('div', null, h1, a)
+,
+content
+
+);
